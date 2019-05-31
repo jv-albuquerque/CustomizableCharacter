5f1b79a19343e1bfb76f78fd180dee11cdfdaba8
João Albuquerque <jv.albuquerque@live.com>
2019-05-31 11:34:34 -0300

    Chage colors created

---
+++ CustomizableCharacter/Assets/Scripts/CharacterSetup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CharacterSetup : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer skin;
+    [SerializeField] private SpriteRenderer body;
+    [SerializeField] private SpriteRenderer hair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    public void SetSkinColor(Color color)
+    {
+        skin.color = color;
+    }
+
+    public void SetBodyColor(Color color)
+    {
+        body.color = color;
+    }
+
+    public void SetHairColor(Color color)
+    {
+        hair.color = color;
+    }
+}
---
+++ CustomizableCharacter/Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPicker : MonoBehaviour
+{
+    [Header ("Panels")]
+    [SerializeField] private GameObject skinPanel = null;
+    [SerializeField] private GameObject bodyPanel = null;
+    [SerializeField] private GameObject hairPanel = null;
+
+    [Header("Colors")]
+    [SerializeField] private Image SelectedSkinColor = null;
+    [SerializeField] private Color[] skinColors = null;
+
+    [SerializeField] private Image SelectedBodyColor = null;
+    [SerializeField] private Color[] bodyColors = null;
+
+    [SerializeField] private Image SelectedHairColor = null;
+    [SerializeField] private Color[] hairColors = null;
+
+    private int skinColor = 0;
+    private int bodyColor = 0;
+    private int hairColor = 0;
+
+    private CharacterSetup character;
+
+    private void Start()
+    {
+        character = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterSetup>();
+
+        skinPanel.SetActive(false);
+        bodyPanel.SetActive(false);
+        hairPanel.SetActive(false);
+
+        character.SetSkinColor(skinColors[skinColor]);
+        character.SetBodyColor(bodyColors[bodyColor]);
+        character.SetHairColor(hairColors[hairColor]);
+
+        SelectedSkinColor.color = skinColors[skinColor];
+        SelectedBodyColor.color = bodyColors[bodyColor];
+        SelectedHairColor.color = hairColors[hairColor];
+    }
+
+    // Open the panel to enable the player to choose the color he wants
+    // Parameter
+    // panel = the name of the panel it will be opened
+    public void OpenPanel(string panel)
+    {
+        switch(panel)
+        {
+            case "skin":
+                skinPanel.SetActive(true);
+                break;
+            case "body":
+                bodyPanel.SetActive(true);
+                break;
+            case "hair":
+                hairPanel.SetActive(true);
+                break;
+        }
+    }
+
+    // Close the panel of the color picker
+    // Parameter
+    // panel = the name of the panel it will be closed
+    public void ClosePanel(string panel)
+    {
+        switch (panel)
+        {
+            case "skin":
+                skinPanel.SetActive(false);
+                break;
+            case "body":
+                bodyPanel.SetActive(false);
+                break;
+            case "hair":
+                hairPanel.SetActive(false);
+                break;
+        }
+    }
+
+    public void SetSkinColor(int index)
+    {
+        skinColor = index;
+
+        SelectedSkinColor.color = skinColors[skinColor];
+        character.SetSkinColor(skinColors[skinColor]);
+    }
+
+    public void SetBodyColor(int index)
+    {
+        bodyColor = index;
+
+        SelectedBodyColor.color = bodyColors[bodyColor];
+        character.SetBodyColor(bodyColors[bodyColor]);
+    }
+
+    public void SetHairColor(int index)
+    {
+        hairColor = index;
+
+        SelectedHairColor.color = hairColors[hairColor];
+        character.SetHairColor(hairColors[hairColor]);
+    }
+}
